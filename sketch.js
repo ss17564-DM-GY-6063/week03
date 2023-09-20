0a25cb7fb246a05394c3aa0e977014eb74586260
shuoshan <ss17564@nyu.edu>
2023-09-20 15:21:33 -0400

    added if function

--- sketch.js
+++ sketch.js
@@ -1,5 +1,32 @@
+// week 03 code
+
+let xpos = 100;
+
 function setup() {
+    createCanvas(600, 600);
 }
 
 function draw() {
+    background(55);
+// rect(xpos, 10, 30, 30);
+// rect(xpos, 50, 30, 30);
+// rect(xpos, 90, 30, 30);
+
+rect(mouseX,mouseY,30,30);
+
+// print(mouseX,mouseY);
+// in java?
+// console.log(mouseX,mouseY);
+
+if (mouseX > width / 2) {
+    if (mouseY > width / 2) {
+        fill(200,10,200);
+    } else{
+        fill(0,255,0);
+    }
+} else{
+    fill(255);
 }
+
+}
+
